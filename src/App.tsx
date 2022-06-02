971551fb98f3758d27b53581e6fc7af93f01746c
kumarpit <kumar.arpit77@yahoo.com>
2022-06-02 00:22:44 -0700

    refactor dir

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
 import './styles/App.css';
-import CountDispatchers from './state/dispatchers';
+import CountDispatchers from './state/counter/dispatchers';
 import { useSelector, useDispatch } from 'react-redux';
-import { CounterStore } from './state/reducer';
+import { CounterStore } from "./state/counter/reducer";
 
 function App() {
   const value = useSelector((state: CounterStore) => state.count);
@@ -11,6 +11,7 @@ function App() {
       <p>{value}</p>
       <button onClick={() => CountDispatchers.increment(dispatch)}>Increment</button>
       <button onClick={() => CountDispatchers.decrement(dispatch)}>Decrement</button>
+      <button>Test API middleware</button>
     </div>
   );
 }
--- src/state/reducer.tsx
+++ src/state/reducer.tsx
@@ -1,34 +1,10 @@
 import { configureStore } from '@reduxjs/toolkit';
+import countReducer from './counter/reducer';
 import loggerMiddleware from './middleware';
-
-
-export type CounterStore = {
-    count: number
-}
 
 export type Actions = {
     type: String,
     payload: Object
-}
-
-const initialState: CounterStore = { count: 0 }
-
-const countReducer = (
-    state: CounterStore = initialState,
-    { type, payload }: Actions
-): CounterStore => {
-    switch (type) {
-        case "INCREMENT":
-            return {
-                ...state,
-                count: state.count + 1
-            }
-        case "DECREMENT":
-            return state.count == 0 ? state :
-            { ...state, count: state.count - 1 }
-        default:
-            return state;
-    }
 }
 
 export default configureStore({
